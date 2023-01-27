68b3c8a7b0da694155afd1795d83c1435bcdc260
unknown <nvyas3095@gmail.com>
2023-01-27 14:42:41 -0800

    Added proper rotational movement for pitch, yaw, and roll directions. Added code suggested by student for InputSystem rotations which makes it look a lot smoother as well.

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -9,6 +9,16 @@ public class PlayerControls : MonoBehaviour
     [SerializeField] float xRange = 10f;
     [SerializeField] float yRange = 7f;
 
+    [SerializeField] float positionPitchFactor = -2f;
+    [SerializeField] float controlPitchFactor = -10f;
+    [SerializeField] float positionYawFactor = 2f;
+    [SerializeField] float controlRollFactor = -10f;
+
+    [SerializeField] private float rotationFactor = 1f;
+
+    float yThrow;
+    float xThrow;
+
     void OnEnable() {
         movement.Enable();
     }
@@ -19,8 +29,14 @@ public class PlayerControls : MonoBehaviour
 
     void Update()
     {
-        float xThrow = movement.ReadValue<Vector2>().x;
-        float yThrow = movement.ReadValue<Vector2>().y;
+        ProcessTranslation();
+        ProcessRotation();
+    }
+
+    void ProcessTranslation()
+    {
+        xThrow = movement.ReadValue<Vector2>().x;
+        yThrow = movement.ReadValue<Vector2>().y;
 
         float xOffset = xThrow * Time.deltaTime * xControlSpeed;
         float rawXPos = transform.localPosition.x + xOffset;
@@ -31,8 +47,27 @@ public class PlayerControls : MonoBehaviour
         float clampedYPos = Mathf.Clamp(rawYPos, -yRange, yRange);
 
         transform.localPosition = new Vector3(
-            clampedXPos, 
+            clampedXPos,
             clampedYPos,
             transform.localPosition.z);
+    }
+
+    void ProcessRotation()
+    {
+        float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+        float pitchDueToControlThrow = yThrow * controlPitchFactor;
+        float pitch = pitchDueToPosition + pitchDueToControlThrow;
+        
+        float yawDueToPosition = transform.localPosition.x * positionYawFactor;
+        float yaw = yawDueToPosition;
+
+        float rollDueToControlThrow = xThrow * controlRollFactor;
+        float roll = rollDueToControlThrow;
+
+        Quaternion targetRotation = Quaternion.Euler(pitch, yaw, roll);
+        transform.localRotation = Quaternion.RotateTowards(
+            transform.localRotation, 
+            targetRotation,
+            rotationFactor);
     }
 }
