72311e14d4309f99b7c26aa02982a2108da21773
unknown <nvyas3095@gmail.com>
2023-03-26 17:57:21 -0700

    Created 5 enemy prefabs of various different sizes, hitpoints, and score. Added enemies into main scene to test if they work as expected. Also added rigid body to each enemy through code upon starting the game.

--- Assets/Scripts/ScoreBoard.cs
+++ Assets/Scripts/ScoreBoard.cs
@@ -4,17 +4,17 @@ using TMPro;
 public class ScoreBoard : MonoBehaviour
 {
     int score;
-    // TMP_Text scoreText;
+    TMP_Text scoreText;
 
     void Start()
     {
-        // scoreText = GetComponent<TMP_Text>();
-        // scoreText.text = "Start";
+        scoreText = GetComponent<TMP_Text>();
+        scoreText.text = "Start";
     }
 
     public void IncreaseScore(int amountToIncrease)
     {
         score += amountToIncrease;
-        // scoreText.text = score.ToString();
+        scoreText.text = score.ToString();
     }
 }
