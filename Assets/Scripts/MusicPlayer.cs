f7f7b94594c32c2462ad77a96967a15ac4f993fe
unknown <nvyas3095@gmail.com>
2023-05-08 19:29:11 -0700

    Added singleton pattern for new music player.

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    void Awake() 
+    {
+        int numMusicPlayer = FindObjectsOfType<MusicPlayer>().Length;
+
+        if (numMusicPlayer > 1)
+        {
+            Destroy(gameObject);
+        }
+        else 
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
